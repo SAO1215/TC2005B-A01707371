59f04436ee37c3fa4f9f97c0b01aebafd74b801f
SAO1215 <A01707371@tec.mx>
2022-03-27 23:51:19 -0600

    preguntas lab

--- Lab18/app.js
+++ Lab18/app.js
@@ -7,11 +7,14 @@ const rutas_lab18 = require('./routes/lab18.routes');
 const rutas_preguntas = require('./routes/preguntas.routes');
 
 const app = express();
-
 const path = require('path');
+const csrf = require('csurf');
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const csrfProtection = csrf();
 
 app.use(session({
     secret: 'ñlknaeañco3pom4ñi3jrcñlawjomxñi3iq3mc4rsejf0438cnf83h4cknh43ui', 
@@ -24,13 +27,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use(csrfProtection); 
+app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+});
+
 app.use('/lab18', rutas_lab18);
 app.use('/preguntas', rutas_preguntas);
 app.use('/users', rutas_usuarios);
 
 app.use((request, response, next) => {
     console.log('Middleware!');
-    response.send('Aqui no hay nada!!!!! Intenta la ruta laboratorio: http://localhost:3000/users/login o http://localhost:3000/preguntas'); //Manda la respuesta
+    response.send('Aqui no hay nada!!!!! Intenta la ruta laboratorio: http://localhost:3000/users/signup o http://localhost:3000/preguntas'); //Manda la respuesta
 });
 
 
